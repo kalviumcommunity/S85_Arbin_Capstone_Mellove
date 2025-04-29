e677c022aa17cfeab1e5d6ff7253e255579e92e6
arbin-mahato <proarbind@gmail.com>
2025-04-29 16:20:35 +0530

    get-api added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./database/db");
 const cors = require("cors");
+const getRoutes = require("./routes/getRoutes");
 require("dotenv").config({ path: "./config/.env" });
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use("/get", getRoutes);
 
 // Basic Route
 app.get("/", (req, res) => {
